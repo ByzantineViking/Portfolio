57b6ea8ce724db59ed6b093f5ba37d569ebe4c8c
saravit1 <teemu.saravirta@aalto.fi>
2020-02-03 17:47:26 +0200

    Initial commit. Added MaterialUI and Sass

---
+++ src/components/LandingPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Button from '@material-ui/core/Button'
+
+
+const LandingPage = (props) => {
+    
+    return(
+        <div>
+            <Button variant="contained" color="primary">
+                Code
+            </Button>
+        </div>
+    )
+}
+
+
+export default LandingPage
